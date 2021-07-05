0e27c599f930b8a4775250f1bafdc4aa2f982c8c
zxwang2019 <53435402+zxwang2019@users.noreply.github.com>
2021-07-05 23:23:02 +0800

    Update work.sh

--- work.sh
+++ work.sh
@@ -1,1 +1,8 @@
-### blog
+### This logbook records the main two-sample MR process ###
+
+mkdir result
+sh run-MR.sh
+cat ./result/*/*MR.result.txt > all.merge.MR.result.txt
+
+perl change.pl all.merge.MR.result.txt > changed.all.merge.MR.result.txt
+perl /home/zxwang/Project/source/14NG/hash.pl IVW.significant.list > IVW.significant.annotated
