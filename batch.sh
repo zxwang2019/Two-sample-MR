5dc13aff8505ed55c40569c141dfa3b7a9d1ee69
zxwang2019 <53435402+zxwang2019@users.noreply.github.com>
2021-07-06 11:51:04 +0800

    Add files via upload

---
+++ batch.sh
@@ -0,0 +1,7 @@
+Pheno=$1
+
+mkdir /home/usrname/result/new.${Pheno}
+
+Rscript  MR_analyse.R /home/usrname/${Pheno}.exposure.txt  HF.outcome.txt  ${Pheno}.MR.result.txt  ${Pheno}.heterogeneity.txt  ${Pheno}.pleiotropy.txt  ${Pheno}.singlesnp.txt  Scatter_plot.${Pheno}.pdf  Forest_plot.${Pheno}.pdf  leave-one-out.${Pheno}.pdf
+
+mv *${Pheno}*  /home/usrname/result/new.${Pheno}
